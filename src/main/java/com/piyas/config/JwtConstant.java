a6a8d751c834328c8644aeb55754b311009fcae7
Piyas <piyasreza31@gmail.com>
2025-05-13 01:05:19 +0600

    init commit

---
+++ src/main/java/com/piyas/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.piyas.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="fjhdufjh9u8irefhsjkdfhsdiuhfiudhfjsdhfkujhsuihsdjfnhyfusadhfgjdhuiw";
+    public static final String JWT_HEADER="Authorization";
+
+}
---
+++ src/main/java/com/piyas/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.piyas.repository;
+
+import com.piyas.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    public User findByEmail(String username);
+
+}

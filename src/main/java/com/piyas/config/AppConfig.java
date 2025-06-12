a87f11d14534e032af62577094e923fc21b60ac3
Piyas <piyasreza31@gmail.com>
2025-06-12 15:17:15 +0600

    init commit

--- src/main/java/com/piyas/config/AppConfig.java
+++ src/main/java/com/piyas/config/AppConfig.java
@@ -45,7 +45,7 @@ public class AppConfig {
                 CorsConfiguration cfg = new CorsConfiguration();
 
                 cfg.setAllowedOrigins(Arrays.asList(
-                        "http://localhost:3000"
+                        "http://localhost:5454"
                 ));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);

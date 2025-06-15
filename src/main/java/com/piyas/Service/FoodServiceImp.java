d37e59cee6120757deb6a7a9b555fda8ce367686
Piyas <piyasreza31@gmail.com>
2025-06-16 03:43:55 +0600

    init commit

--- src/main/java/com/piyas/Service/FoodServiceImp.java
+++ src/main/java/com/piyas/Service/FoodServiceImp.java
@@ -8,6 +8,7 @@ import com.piyas.request.CreateFoodRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -32,6 +33,7 @@ public class FoodServiceImp implements FoodService{
         food.setIngredients(req.getIngredients());
         food.setVegetarian(req.isVegetarian());
         food.setSeasonal(req.isSeasonal());
+        food.setCreationDate(new Date());
 
         Food savedFood= foodRepository.save(food);
         restaurant.getFoods().add(savedFood);
